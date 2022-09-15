e352bc79411be479c281aab19d6a18f052726742
Darley Rodrigues <darleyramos96@gmail.com>
2022-09-14 21:10:56 -0300

    Desafio dia 03

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Code1 from './Code1'
+import Code2 from './Code2'
 
 ReactDOM.render(
-    <h1>
-      <Code1></Code1>
-    </h1>,
+    <div>
+      <Code1/>,
+      <Code2/>,
+    </div>,
     document.getElementById('root')
 )
